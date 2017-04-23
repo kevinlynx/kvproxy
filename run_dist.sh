1c145765bfc32c0e283526d24362bf1623575230
Kevin Lynx <kevinlynx@gmail.com>
2017-04-23 21:11:19 +0800

    add gc stat

--- run_dist.sh
+++ run_dist.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-java -cp kvproxy-1.0-SNAPSHOT.jar:lib/* -Dlog4j.configuration=file:log4j.properties com.codemacro.kvproxy.App
+java -cp kvproxy-1.0-SNAPSHOT.jar:lib/* -Dlog4j.configuration=file:log4j.properties \
+    -Xloggc:gc.log -XX:+PrintGCTimeStamps -XX:+PrintGCDetails \
+        com.codemacro.kvproxy.App
